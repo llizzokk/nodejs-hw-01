9176d8d2ea5e9e2af9f41dba38b93c7e0ee3fd5a
Liza <lizaapostol378@gmail.com>
2025-02-21 21:22:56 +0200

    update task

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const newContact = createFakeContact();
+
+    const updatedContacts = [...contacts, newContact];
+
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 addOneContact();
